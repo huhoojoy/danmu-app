eac2cfdf63a3203726d2dd6ca6964f669dc2ac29
huhoowork <617348792@qq.com>
2017-03-01 14:13:23 +0800

    move jQuery.js to the same folder of danmupage.html, add a click event handler to assign the val of input to the danmubox, add a pseudo-element to css file, fixed some bugs.

--- danmuapp.js
+++ danmuapp.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-  $('input').val();
+  $('.btn-fire').on('click', function() {
+    var inputCon = $('input').val();
+    if(inputCon === "") {
+      alert('请先装弹再发射！');
+    } else {
+      $('.words').text(inputCon);
+      $('.words:after').css({'visibility':'visible'});
+      //some problem of understanding the pseudo-element and animation
+    };
+
+    $('.btn-fire').css({'color': 'darkblue'});
+
+  });
+
 });
