84e960d165b4098fd6d475c251a3d4007f2aa2fa
huhoowork <617348792@qq.com>
2017-03-06 17:09:42 +0800

    add Wilddog cloud to html and Js file

--- danmuapp.js
+++ danmuapp.js
@@ -1,10 +1,20 @@
 $(document).ready(function() {
+
+  var ref = new Wilddog("https://huhoo-danmuapp.wilddogio.com/danmu");
+  var arr = [];
+//将数据上传到野狗云.
+  $(".btn-fire").click(function() {
+  	    var inputCon = $('input').val();
+  	    ref.child('danmu').push(inputCon);
+  	    $('input').val('');
+  	  });
+
+
   $('.btn-fire').on('click', function() {
-    var inputCon = $('input').val();
-    if(inputCon === "") {
+    if($('input').val() === '') {
       alert('请先装弹再发射！');
     } else {
-      $('.words').text(inputCon);
+      $('.words').text($('input').val());
     };
   });
 
